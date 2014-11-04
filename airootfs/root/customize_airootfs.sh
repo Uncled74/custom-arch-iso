4d9d60bb990c542c1162e4d5f87e864d7a429dd8
studersi <mail@studer.si>
2014-11-04 16:15:35 +0100

    Change profile file to zsh instead of bash.

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -32,4 +32,4 @@ echo exec startxfce4 >> /home/simu/.xinitrc
 
 rm /etc/systemd/system/getty@tty1.service.d/autologin.conf
 
-echo "[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx" >> /home/simu/.bash_profile
+echo "[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx" >> /home/simu/.zprofile
